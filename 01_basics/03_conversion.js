8bcbc2d388836257203ab2e68c95093f16410961
piyushkumarchaurasiya302-debug <piyushkumarchaurasiya302@gmail.com>
2026-03-02 20:13:16 +0000

    conversion_od_datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,10 @@
+let score = "33abc"
+
+console.log(typeof(score))
+
+let Val = Number(score) 
+console.log(typeof(Val))
+console.log(Val)
+
+//  "33" => 33 
+//  "33aba" => NaN => its type is number
