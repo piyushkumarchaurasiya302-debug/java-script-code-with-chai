30774df387c7ee4d085ab4ccaa04711150825bff
piyushkumarchaurasiya302-debug <piyushkumarchaurasiya302@gmail.com>
2026-03-02 19:23:10 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" ; // treat all js newer version 
+
+// code redability should be high
+
+// alert(3 + 3) //  we are using node js not browser 
+
+console.log(3 + 3) 
+
+console.log("piyush")
+
+let name = "Piyush" 
+let age = 18 
+let isLoggedIn = false
+
+
+// number => 2 to the power 53 
+//bigint 
+// string => " "
+// boolean 
+// null => standalone value 
+
+// undefined 
+// symbol=> unique 
+
+// object 
+console.log(typeof null)
+console.log(typeof undefined)
