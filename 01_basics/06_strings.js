135a6e0a4ebe555600a77962100a7c544a83d41b
piyushkumarchaurasiya302-debug <piyushkumarchaurasiya302@gmail.com>
2026-03-03 13:38:06 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,35 @@
+// const name = "Piyush"
+
+// const repocount = 20 
+
+// console.log(name + repocount + "value")
+// console.log(`hello my  name is ${name}  and my repocount is ${repocount}`)
+
+const GameName = new String('piyush')
+
+// console.log(GameName[0])
+// console.log(GameName.length)
+// console.log(GameName.toUpperCase)
+// console.log(GameName.charAt(2))
+// console.log(GameName.indexOf('y'))
+
+
+// const  newstring = GameName.substring(0,5)
+
+// console.log(newstring)
+
+// const  newstring = GameName.slice(0,5)    // follows negative indexing
+
+// console.log(newstring)
+
+const newstring = "    piyush    "
+
+console.log(newstring)
+console.log(newstring.trim())     // trims the extra space
+
+//consol.log(url.replace('old substring' , 'new substring')) 
+
+console.log(url.include())    //used to check that given substring is present in the string or not
+
+console.log(GameName.split('-'))
+
