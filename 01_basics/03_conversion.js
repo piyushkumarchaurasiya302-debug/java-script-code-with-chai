aaf3f13faf99bba20dc27129353a65a8f8ad3061
piyushkumarchaurasiya302-debug <piyushkumarchaurasiya302@gmail.com>
2026-03-03 11:20:59 +0000

    conversions

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,10 +1,28 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof(score))
+// console.log(typeof(score))
 
-let Val = Number(score) 
-console.log(typeof(Val))
-console.log(Val)
+// let Val = Number(score) 
+// console.log(typeof(Val))
+// console.log(Val)
 
 //  "33" => 33 
-//  "33aba" => NaN => its type is number
+//  "33aba" => NaN => its type i 
+
+// operations 
+
+// let val = 3 
+
+// let negval = -val
+
+// console.log(negval)
+
+console.log("1" + 2) ;
+console.log(1 + "2") 
+console.log(1 + 1 + "3") 
+console.log("1" +2 + 2)
+
+console.log(true)
+console.log(+true) 
+
+console.log(+"")
