7c54749aa40b7088c54d9ecce73ed05a701d30dd
piyushkumarchaurasiya302-debug <piyushkumarchaurasiya302@gmail.com>
2026-03-03 11:20:07 +0000

    datatypes_summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,22 @@
+// primitive datatypes => 7
+//String , Number , Boolean , null , undefined ,Symbol , BigInt 
+
+let username  ;
+
+const id = Symbol('123')
+const anotherId = Symbol('123') 
+
+console.log(id === anotherId) 
+
+const bigNumber = 2618686313834484538n
+
+// reference (non primitive)
+
+// array , object , functions  
+
+const heros = ["ajay" , "vijay" , "sanjay"]
+
+let myObj = {
+    name : "piyush " ,
+    id : 20253188 ,
+}
