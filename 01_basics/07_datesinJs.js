b393598532815dc42a0f1cb89d4e6a930c66db6b
piyushkumarchaurasiya302-debug <piyushkumarchaurasiya302@gmail.com>
2026-03-04 18:52:46 +0000

    dates in java script

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,31 @@
+// Dates 
+
+// let mydate = new Date()
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleString())
+
+// console.log(typeof mydate)
+
+// let created = new Date(2023 , 0 , 23)
+// console.log(created.toDateString())
+
+// let created = new Date(2023 , 0 , 23 ,5 ,3)
+// console.log(created.toLocaleSteing())
+
+// let createDate = new Date("01-11-2025")
+// // console.log(createDate.toLocaleString())
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)
+
+// console.log(createDate.getTime())
+
+let newDate = new Date() ;
+
+console.log(newDate.getDay())
+console.log(newDate.getMonth() + 1)
+
+
+
