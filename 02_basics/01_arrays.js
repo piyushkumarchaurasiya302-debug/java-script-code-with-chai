fa6816123eef1780222de47174883362ccda8899
piyushkumarchaurasiya302-debug <piyushkumarchaurasiya302@gmail.com>
2026-03-10 06:38:45 +0000

    arrays 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,43 @@
+// ARRAYS 
+
+const myarray = [0,1,2,3,4,5]
+// const myHeros = ["batman" , "modi" , "rahul "]
+
+// const newArray = new Array(1,2,3,4,5,6)
+
+// console.log(myarray[1])
+
+
+/// arrys methods  
+// myarray.push(10)
+// myarray.push(13)
+
+// myarray.pop()
+
+// myarray.unshift(8)
+// myarray.shift()
+
+// console.log(myarray)
+// console.log(myarray.includes(5))
+// console.log(myarray.indexOf(5))
+
+
+// const newArray = myarray.join()
+
+// console.log(myarray)
+// console.log(newArray)
+
+// console.log(typeof newArray)
+
+// ******* slice and splice ******* // 
+
+console.log("A" , myarray)
+const mn1 = myarray.slice(1,3);
+
+console.log(mn1)
+console.log("B" , myarray)
+
+const mn2 = myarray.splice(1,3);
+
+console.log(mn2)
+console.log("c" , myarray)
